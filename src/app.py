c66ed7630381ed029c34c0bc9973e914e1001af3
Yuki Sakamoto <smoto_orylab@orymcba.local>
2025-05-27 09:41:43 +0900

    initial commit

---
+++ src/app.py
@@ -0,0 +1,9 @@
+# src/app.py の冒頭
+import streamlit as st
+# from scraping.url_helpers import fetch_url
+
+st.title("URL 取得テスト")
+# url = st.text_input("URL を入力")
+# if st.button("取得"):
+#     html = fetch_url(url)
+#     st.code(html[:200] + "…", language="html")
